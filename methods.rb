c2643127867c713f0b4e33799c798fee1291f582
Tina Shi <init.defaultBranch>
2023-05-01 20:03:55 +1000

    commit tree 3a7c5c343bd9d186bce1754f3c04bb6cefa4289e

--- methods.rb
+++ methods.rb
@@ -1,1 +1,31 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet name 
+    puts "Hello, #{name}!"
+end
+
+greet("Naureen")
+greet("Jimmy")
+
+def greet_with_default name = "programmer"
+    puts "Hello, #{name}!"
+end
+
+def add num1, num2
+    return num1 + num2
+end
+
+add(2,5)
+
+def halve number
+    if number.class != Integer
+        return nil;
+    end
+    
+    return number / 2;
+end
+
+halve(6);
